0ce4ae35635ba1f5d56f453233aa5302fe89d4cb
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2022-06-18 14:24:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 #  *                                                                                                                                                   *
 #  * Copyright © 2015 - present. MongoExpUser.  All Rights Reserved.                                                                                   *
 #  *                                                                                                                                                   *
-#  * License: MIT - https://github.com/MongoExpUser/UNEPO-Stack-on-AWS-Lightsail/blob/main/README.md                                                   *
+#  * License: MIT - https://github.com/MongoExpUser/UNEPO-Stack-on-AWS-Lightsail/blob/main/LICENSE                                                     *
 #  *                                                                                                                                                   *
 #  * @License Ends                                                                                                                                     *
 #  *****************************************************************************************************************************************************
